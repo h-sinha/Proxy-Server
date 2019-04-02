944373b973be15c08d5428bf6eb7938096eddcca
Harsh <harsh.26020@gmail.com>
2019-04-02 12:20:57 +0530

    Added proxy server

--- proxy_server/server.py
+++ proxy_server/server.py
@@ -6,5 +6,21 @@ proxy_socket.bind(('', 20100))
 proxy_socket.listen(10)
 
 while True:
+    # print proxy_socket.accept()
     client_socket, client_addr = proxy_socket.accept()
-    print client_socket, client_addr
+    print (client_socket, client_addr)
+    request = client_socket.recv(1024)
+    print (request)
+    http_response = '''HTTP/1.1 200 OK
+Content-Length: 88
+Content-Type: text/html
+<html>
+<body>
+<h1>Hello, World!</h1>
+</body>
+</html>    
+'''
+    client_socket.send(http_response.encode())
+    client_socket.close()
+    # ct = client_thread(clientsocket)
+    # ct.run()
