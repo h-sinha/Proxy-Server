22b970e8bc401dd2576fa0f62fef3cd0ff16d505
Anurag Jain <anurag.jain@research.iiit.ac.in>
2019-04-02 21:48:43 +0530

    Blacklist

--- proxy_server/server.py
+++ proxy_server/server.py
@@ -1,15 +1,25 @@
 import socket 
 import threading
-
 proxy_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 proxy_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 proxy_socket.bind(('', 20100))
 proxy_socket.listen(10)
+data = []
+with open("blacklist.txt", "r") as f:
+    data = f.readlines()
+    for i in range(len(data)):
+        data[i] = data[i].strip()
+
+def is_Blocked(host):
+    for x in data:
+        if x in host:
+            return True
+    return False
 
 def get_request(client_socket, client_addr):
     # print (client_socket, client_addr)
     request = client_socket.recv(1024)
-    # print(request)
+    print(request)
     headers = request.decode('ascii').split('\r\n')
     http_header = {}
     for i in range(len(headers)):
@@ -17,6 +27,17 @@ def get_request(client_socket, client_addr):
         if split_data[0] == 'Host':
             http_header['Host'] = split_data[1][1:]
     print(http_header['Host'])
+    if is_Blocked(http_header['Host']):
+        http_response = '''HTTP/1.1 200 OK
+Content-Type: text/html   
+\r\n<html>
+<body>
+<h1>The host you are trying to connect is Blacklisted!</h1>
+</body>
+</html>
+'''
+        client_socket.send(http_response.encode('ascii'))
+        client_socket.close()
     http_response = '''HTTP/1.1 200 OK
 Content-Type: text/html   
 \r\n<html>
