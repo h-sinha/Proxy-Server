5c5da2b1eca8b374946bf9384307e35df76ba095
Harsh <harsh.26020@gmail.com>
2019-04-03 20:12:43 +0530

    Added client sim

--- http_server/client_sim.py
+++ http_server/client_sim.py
@@ -1,10 +1,13 @@
 import random
 import os
-port = random.randint(20000, 20099)
+import sys
+import time
 request_type = ['GET', 'POST']
-f = 1
-while f:
-	cur_type = request_type[random.randint(1, 10) % 2]
-	server_port = random.randint(20101, 20200)
-	os.system('curl -X %s --local-port %s --proxy http://admin:admin@0.0.0.0:20100 http://0.0.0.0:20196/cgi-bin/env.cgi'% (cur_type, port))
-	f = 0
+for i in range(20000, 200100):
+	pid = os.fork()
+	if pid == 0:
+		while True:
+			cur_type = request_type[random.randint(1, 10) % 2]
+			server_port = random.randint(20101, 20200)
+			os.system('curl -X %s --local-port %s --proxy http://admin:admin@0.0.0.0:20100 http://0.0.0.0:%s/cgi-bin/env.cgi'% (cur_type, i, server_port))
+			time.sleep(5)
