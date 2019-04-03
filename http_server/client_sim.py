f0527c93afb3f878ce7de34c5512c08effa8d0bc
Harsh <harsh.26020@gmail.com>
2019-04-03 21:23:44 +0530

    UPdated client_sim

--- http_server/client_sim.py
+++ http_server/client_sim.py
@@ -3,11 +3,11 @@ import os
 import sys
 import time
 request_type = ['GET', 'POST']
-for i in range(20000, 200100):
+for i in range(20000, 20100):
 	pid = os.fork()
 	if pid == 0:
 		while True:
 			cur_type = request_type[random.randint(1, 10) % 2]
 			server_port = random.randint(20101, 20200)
-			os.system('curl -X %s --local-port %s --proxy http://admin:admin@0.0.0.0:20100 http://0.0.0.0:%s/cgi-bin/env.cgi'% (cur_type, i, server_port))
-			time.sleep(5)
+			os.system('curl -X %s --local-port %s --proxy http://harsh:pass@0.0.0.0:20100 http://0.0.0.0:%s/cgi-bin/env.cgi'% (cur_type, i, server_port))
+			time.sleep(random.randint(1,100))
